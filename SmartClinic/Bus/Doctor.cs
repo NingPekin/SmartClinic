b024f2ecc3f6167fb47fde70d5d1dd9a7409c8a7
Fary <farzaneh.fallahi@gmail.com>
2018-04-05 13:21:41 -0400

    Id, Name and Category properties added to the Doctor class

--- SmartClinic/Bus/Doctor.cs
+++ SmartClinic/Bus/Doctor.cs
@@ -7,5 +7,13 @@ namespace SmartClinic.Bus
 {
     public class Doctor
     {
+        private string id;
+        private string name;
+        private string category;
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Category { get => category; set => category = value; }
+
     }
 }
